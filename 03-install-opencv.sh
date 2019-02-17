b89e240cfc927952d67ec2e3cf19a0dc06890319
micais2019 <abachman@mica.edu>
2019-02-17 18:44:44 -0500

    more support packages

--- 03-install-opencv.sh
+++ 03-install-opencv.sh
@@ -4,7 +4,8 @@
 # wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.4.5.zip
 # unzip opencv_contrib.zip
 
-sudo apt install -y  --fix-broken libsz2 libatk1.0-0 libatlas3-base libavcodec57 libavformat57 \
+sudo apt install -y  --fix-broken libqtgui4 libqt4-test libcblas3 libsz2 \
+  libatk1.0-0 libatlas3-base libavcodec57 libavformat57 \
   libavutil55 libcairo-gobject2 libcairo2 libgdk-pixbuf2.0-0 libgstreamer1.0-0 \
   libgtk-3-0 libharfbuzz0b libhdf5-100 libilmbase12 libjasper1 libopenexr22 \
   libpango-1.0-0 libpangocairo-1.0-0 libswscale4 libtiff5 libwebp6
