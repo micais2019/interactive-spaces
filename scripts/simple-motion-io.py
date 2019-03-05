4c45de39ad2a6cb8c034753fc0e172d87c97e670
abachman <abachman@mica.edu>
2019-03-04 19:37:06 -0500

    add neopixel circuitpython install

--- scripts/simple-motion-io.py
+++ scripts/simple-motion-io.py
@@ -4,24 +4,7 @@
 #####################
 import numpy as np
 import cv2
-
-#####################
-## Adafruit IO
-#####################
-from Adafruit_IO import Client
-from datetime import datetime
-import os
-
-ADAFRUIT_IO_USERNAME = os.environ.get('ADAFRUIT_IO_USERNAME')
-ADAFRUIT_IO_KEY = os.environ.get('ADAFRUIT_IO_KEY')
-
-if ADAFRUIT_IO_USERNAME == None or ADAFRUIT_IO_KEY == None:
-    print("make sure ADAFRUIT_IO_USERNAME and ADAFRUIT_IO_KEY are set as environment variables")
-    exit(1)
-
-## setup Adafruit IO client
-aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
-motion = aio.feeds('motion')
+import random
 
 ## setup motion detection support functions
 sdThresh = 10
@@ -36,6 +19,60 @@ def distMap(frame1, frame2):
     dist = np.uint8(norm32*255)
     return dist
 
+# like processing or arduino's "map" function. Maps a value from left range to right range.
+def linMap(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+
+
+#####################
+## Adafruit IO
+#####################
+from Adafruit_IO import Client
+from datetime import datetime
+import os
+
+ADAFRUIT_IO_USERNAME="ab_home"
+ADAFRUIT_IO_KEY="3a9f2b3bac204522a133fc4494c983ad"
+# ADAFRUIT_IO_USERNAME = os.environ.get('ADAFRUIT_IO_USERNAME')
+# ADAFRUIT_IO_KEY = os.environ.get('ADAFRUIT_IO_KEY')
+
+if ADAFRUIT_IO_USERNAME == None or ADAFRUIT_IO_KEY == None:
+    print("make sure ADAFRUIT_IO_USERNAME and ADAFRUIT_IO_KEY are set as environment variables")
+    exit(1)
+
+## setup Adafruit IO client
+aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+motion = aio.feeds('motion-2')
+
+#######
+## LEDs
+####### 
+import board
+import neopixel
+pixels = neopixel.NeoPixel(board.D18, 12)
+light_on = False
+
+
+### script specific variables
+counter = 0
+last_pub = datetime.utcnow()
+publish_delay_seconds = 10
+light_on_delay_seconds = 1
+on_color = (255, 255, 0)
+off_color = (0, 0, 0)
+
+
+pixels.fill(on_color)
+
 #capture video stream from camera source. 0 refers to first camera, 1 referes to 2nd and so on.
 cap = cv2.VideoCapture(0)
 
@@ -43,12 +80,11 @@ cap = cv2.VideoCapture(0)
 _, frame1 = cap.read()
 _, frame2 = cap.read()
 
-counter = 0
-last_pub = datetime.utcnow()
-publish_delay_seconds = 30
+ 
+pixels.fill(off_color)
 
 while(True):
-
+    
     # take a new frame of video
     _, frame3 = cap.read()
     rows, cols, _ = np.shape(frame3)
@@ -70,15 +106,19 @@ while(True):
     # calculate standard deviation test
     _, stDev = cv2.meanStdDev(mod)
 
+    pcount = linMap(stDev[0][0], 10, 50, 0, 12)
+    for i in range(12):
+        if pcount > i:
+            pixels[i] = on_color
+        else:
+            pixels[i] = off_color
+
     # render text to the screen
     cv2.putText(frame2, "Standard Deviation - {}".format(round(stDev[0][0],0)), (70, 70), font, 1, (255, 0, 255), 1, cv2.LINE_AA)
     if stDev > sdThresh:
         print("motion detected {:04d}".format(counter));
         cv2.imshow('motion', mod)
-        # cv2.imwrite('/home/pi/images/dist-{:04d}.jpg'.format(counter), mod)
-        # cv2.imwrite('/home/pi/images/frame-{:04d}.jpg'.format(counter), frame2)
-        # cv2.imwrite('/home/pi/images/frame-{:04d}.jpg'.format(counter), mod)
-
+        
         # increase the counter value when motion is detected in a frame
         counter = counter + 1
 
@@ -92,8 +132,16 @@ while(True):
         print('')
         aio.send_data(motion.key, counter)
         counter = 0
+        light_on = True
+        on_color = (int(random.random() * 255), int(random.random() * 255), int(random.random() * 255))
+    
         last_pub = now
 
+    # if light_on:
+    #    pixels.fill(on_color)
+    #    if (now - last_pub).seconds > light_on_delay_seconds:
+    #        light_on = False
+    #        pixels.fill((0, 0, 0))
 
     # cv2.imwrite('/home/pi/images/frame.jpg', mod)
     if cv2.waitKey(1) & 0xFF == 27:
