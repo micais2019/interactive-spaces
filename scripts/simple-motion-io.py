a22270613552df29c79b0051c63582a600e51706
Adam Bachman <adam.bachman@gmail.com>
2019-03-09 11:24:18 -0500

    reorg simple-motion

--- scripts/simple-motion-io.py
+++ scripts/simple-motion-io.py
@@ -4,6 +4,33 @@
 #####################
 import numpy as np
 import cv2
+import random
+
+## setup motion detection support functions
+sdThresh = 10
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+def distMap(frame1, frame2):
+    """outputs pythagorean distance between two frames"""
+    frame1_32 = np.float32(frame1)
+    frame2_32 = np.float32(frame2)
+    diff32 = frame1_32 - frame2_32
+    norm32 = np.sqrt(diff32[:,:,0]**2 + diff32[:,:,1]**2 + diff32[:,:,2]**2)/np.sqrt(255**2 + 255**2 + 255**2)
+    dist = np.uint8(norm32*255)
+    return dist
+
+def linMap(value, leftMin, leftMax, rightMin, rightMax):
+    """maps a value from one range onto another"""
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
 
 #####################
 ## Adafruit IO
@@ -25,20 +52,32 @@ if ADAFRUIT_IO_USERNAME == None or ADAFRUIT_IO_KEY == None:
 
 ## setup Adafruit IO client
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
-motion = aio.feeds('motion')
+motion = aio.feeds('motion-2')
 
-## setup motion detection support functions
-sdThresh = 10
-font = cv2.FONT_HERSHEY_SIMPLEX
 
-def distMap(frame1, frame2):
-    """outputs pythagorean distance between two frames"""
-    frame1_32 = np.float32(frame1)
-    frame2_32 = np.float32(frame2)
-    diff32 = frame1_32 - frame2_32
-    norm32 = np.sqrt(diff32[:,:,0]**2 + diff32[:,:,1]**2 + diff32[:,:,2]**2)/np.sqrt(255**2 + 255**2 + 255**2)
-    dist = np.uint8(norm32*255)
-    return dist
+#######
+## LEDs
+#######
+## load libraries:
+##
+##   sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
+##
+import board
+import neopixel
+pixels = neopixel.NeoPixel(board.D18, 12)
+light_on = False
+
+
+### script specific variables
+max_motion_value = 0
+last_pub = datetime.utcnow()
+publish_delay_seconds = 10
+light_on_delay_seconds = 1
+on_color = (255, 255, 0)
+off_color = (0, 0, 0)
+
+
+pixels.fill(on_color)
 
 #capture video stream from camera source. 0 refers to first camera, 1 referes to 2nd and so on.
 cap = cv2.VideoCapture(0)
@@ -47,9 +86,8 @@ cap = cv2.VideoCapture(0)
 _, frame1 = cap.read()
 _, frame2 = cap.read()
 
-counter = 0
-last_pub = datetime.utcnow()
-publish_delay_seconds = 30
+
+pixels.fill(off_color)
 
 while(True):
 
@@ -74,30 +112,41 @@ while(True):
     # calculate standard deviation test
     _, stDev = cv2.meanStdDev(mod)
 
+    max_motion_value = max(stDev, max_motion_value)
+
+    pcount = linMap(stDev[0][0], 10, 50, 0, 12)
+    for i in range(12):
+        if pcount > i:
+            pixels[i] = on_color
+        else:
+            pixels[i] = off_color
+
     # render text to the screen
     cv2.putText(frame2, "Standard Deviation - {}".format(round(stDev[0][0],0)), (70, 70), font, 1, (255, 0, 255), 1, cv2.LINE_AA)
     if stDev > sdThresh:
         print("motion detected {:04d}".format(counter));
         cv2.imshow('motion', mod)
-        # cv2.imwrite('/home/pi/images/dist-{:04d}.jpg'.format(counter), mod)
-        # cv2.imwrite('/home/pi/images/frame-{:04d}.jpg'.format(counter), frame2)
-        # cv2.imwrite('/home/pi/images/frame-{:04d}.jpg'.format(counter), mod)
-
-        # increase the counter value when motion is detected in a frame
-        counter = counter + 1
 
     # send accumulated data every publish_delay_seconds
     now = datetime.utcnow()
     if (now - last_pub).seconds > publish_delay_seconds:
         print('')
         print("------------------------ {}".format(now))
-        print("motion published {:04d}".format(counter));
+        print("motion published {:04d}".format(max_motion_value));
         print("------------------------------".format(now))
         print('')
         aio.send_data(motion.key, counter)
-        counter = 0
+        max_motion_value = 0
+        light_on = True
+        on_color = (int(random.random() * 255), int(random.random() * 255), int(random.random() * 255))
+
         last_pub = now
 
+    # if light_on:
+    #    pixels.fill(on_color)
+    #    if (now - last_pub).seconds > light_on_delay_seconds:
+    #        light_on = False
+    #        pixels.fill((0, 0, 0))
 
     # cv2.imwrite('/home/pi/images/frame.jpg', mod)
     if cv2.waitKey(1) & 0xFF == 27:
