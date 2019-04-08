8e6a67f5612acff602f7f629c841bdc1f530da53
micais2019 <abachman@mica.edu>
2019-04-08 14:39:33 -0400

    better dotstar checks, fixed screen_writer, add demo code for storing a frame from the camera

--- scripts/sensor-test/dotstar-test.py
+++ scripts/sensor-test/dotstar-test.py
@@ -4,12 +4,17 @@
 import time
 import board
 import adafruit_dotstar as dotstar
-dots = dotstar.DotStar(board.SCK, board.MOSI, 8, brightness=0.8)
+DOTCOUNT = 192
+dots = dotstar.DotStar(board.SCK, board.MOSI, DOTCOUNT, brightness=0.8)
 
 i = 0
 while True:
-    dots[i] = (255, 0, 0) 
-    time.sleep(0.05) 
-    dots[i] = (0, 0, 0)
+    dots.fill((255, 0, 0))
 
-    i = (i + 1) % 8
+    time.sleep(0.2) 
+
+    dots.fill((0, 0, 0))
+
+    time.sleep(0.3) 
+
+    # i = (i + 1) % DOTCOUNT
