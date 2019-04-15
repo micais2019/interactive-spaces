6c0d70fe6d6c380ee91b0f72bf4203f5bbbea923
Adam Bachman <adam.bachman@gmail.com>
2019-04-15 02:06:36 +0100

    fix data_sender

--- scripts/utils/data_sender.py
+++ scripts/utils/data_sender.py
@@ -1,23 +1,31 @@
 from requests_futures.sessions import FuturesSession
+import json
 
 class DataSender:
-    def __init__(self, username, key):
+    def __init__(self, username, key, debug=False):
         self.session = FuturesSession()
         self.headers = {
             'User-Agent': 'mica-vibe/0.0.1',
             'Content-Type': 'application/json',
             'X-AIO-Key': key
         }
+        self.debug = debug
         self.base_url = "https://io.adafruit.com/api/v2/{}/feeds".format(username)
+        self.session.hooks['response'] = self.response_hook
 
     def send_data(self, feed, value):
         suffix = "/{}/data".format(feed)
         full_url = self.base_url + suffix
-        print("POST {}".format(full_url))
-        self.session.post(full_url, data={"value": value}, hooks={
-            'response', self.response_hook
-        })
+        if self.debug:
+            print("[DataSender send_data] POST {}".format(full_url))
+        return self.session.post(full_url, headers=self.headers, data=json.dumps({"value": value}))
 
     def response_hook(self, resp, *args, **kwargs):
-        # parse the json storing the result on the response object
-        print("GOT {} RESPONSE JSON: {}".format(resp.status_code, resp.json()))
+        if self.debug:
+            print("[DataSender send_data] got response status: {}".format(resp.status_code))
+
+if __name__=="__main__":
+    import time
+
+    ds = DataSender('mica_ia', '3a9f2b3bac204522a133fc4494c983ad', debug=True)
+    req = ds.send_data('mood', 'test')
