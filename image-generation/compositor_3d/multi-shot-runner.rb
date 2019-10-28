c9af16d0cb4e0488a8bc6835d65e0accb104ec81
Adam Bachman <adam.bachman@gmail.com>
2019-10-28 11:33:09 -0400

    make stages toggleable, add cropping, add PDF bundling

--- image-generation/compositor_3d/multi-shot-runner.rb
+++ image-generation/compositor_3d/multi-shot-runner.rb
@@ -5,71 +5,112 @@ require 'concurrent'
 start = Time.now
 PATH = "/Users/adam/projects/MICA/interactive-spaces-code/image-generation/compositor_3d"
 
-puts "GENERATING"
+GENERATE_IMAGES = false
+CONVERT_IMAGES = true
+BUNDLE_PDF = true
+
+BUNDLE_GROUPS_OF = 10
 
 generator = Concurrent::FixedThreadPool.new(1)
 
-10.times do |n|
-  start_index = (n * 100) + 1
-  puts "LAUNCH #{start_index}"
+if GENERATE_IMAGES
+  puts "GENERATING"
+  # 10.times do |n|
+    start_index = 13001
+    puts "LAUNCH #{start_index}"
 
-  generator << Proc.new do
-    begin
-      cmd = "processing-java --sketch=#{PATH} --run #{start_index}"
-      system(cmd)
-    rescue => ex
-      puts "ERROR #{ex}"
+    generator << Proc.new do
+      begin
+        cmd = "processing-java --sketch=#{PATH} --run #{start_index}"
+        system(cmd)
+      rescue => ex
+        puts "ERROR #{ex}"
+      end
+    end
+  # end
+
+  generator.shutdown
+  generator.wait_for_termination
+
+  msg = "|  RUN TIME #{Time.now.to_f - start.to_f} SECONDS  |"
+
+  puts
+  puts "-" * msg.size
+  puts msg
+  puts "-" * msg.size
+  puts
+end
+
+if CONVERT_IMAGES
+  # organize
+  output_path = File.join(PATH, "output/*.png")
+  puts "APPLYING CROPS #{output_path}"
+
+  converter = Concurrent::FixedThreadPool.new(8)
+
+  Dir[output_path].each do |image|
+    converter << Proc.new do
+      # # optimize
+      # cmd = "optipng -zc 9 -zm 8 -zs 0 -f 5 -o1 #{image} >> optipng.log"
+      # system(cmd)
+
+      fn = File.basename(image)
+      name, ftype = fn.split('.')
+      time, idx, _, _ = name.split('_')
+      crop_name = File.join PATH, "crop", "%i_%i_4240_3000.%s" % [time, idx, ftype]
+      print_name = File.join PATH, "print", "%i_%i_4240_3000.tiff" % [time, idx]
+
+      # overlay
+      if !File.exists?(crop_name)
+        puts "add crops to #{image}"
+        overlay_command = "convert -size 4240x3000 xc:white \\( #{image} \\) -geometry +77+77 -composite \\( data/crops.png \\) -geometry +0+0 -composite #{crop_name}"
+        system(overlay_command)
+      end
+
+      # colorspace
+      if !File.exists?(print_name)
+        puts "convert colorspace of #{crop_name}"
+        convert_command = "convert #{crop_name} -profile data/sRGB2014.icc -profile data/psocoated-v3.icc #{print_name}"
+        system(convert_command)
+      end
     end
   end
+
+  converter.shutdown
+  converter.wait_for_termination
 end
 
-generator.shutdown
-generator.wait_for_termination
+if BUNDLE_PDF
+  print_path = File.join(PATH, "print/*.tiff")
 
-msg = "|  RUN TIME #{Time.now.to_f - start.to_f} SECONDS  |"
+  bundler = Concurrent::FixedThreadPool.new(2)
 
-puts
-puts "-" * msg.size
-puts msg
-puts "-" * msg.size
-puts
+  Dir[print_path].each_slice(BUNDLE_GROUPS_OF) do |slice|
+    bundler << Proc.new do
+      puts "BUNDLING #{slice.size} FILES"
+      sorted = slice.sort
 
-# organize
-output_dir = File.join(PATH, "output/*.png")
-puts "OPTIMIZING #{output_dir}"
+      first_file = File.basename(sorted.first)
+      last_file  = File.basename(sorted.last)
 
-optimizer = Concurrent::FixedThreadPool.new(8)
+      start_idx  = first_file.split('_')[1]
+      end_idx    = last_file.split('_')[1]
 
-Dir[output_dir].each do |image|
-  optimizer << Proc.new do
-    #  -zc levels
-    #         Select the zlib compression levels used in IDAT compression.
-    #         The  levels  argument  is  specified  as a rangeset (e.g. -zc6-9), and the default levels value depends on the optimization
-    #         level set by the option -o.
-    #         The effect of this option is defined by the zlib(3) library used by OptiPNG.
-    #  -zm levels
-    #         Select the zlib memory levels used in IDAT compression.
-    #         The levels argument is specified as a rangeset (e.g. -zm8-9), and the default levels  value  depends  on  the  optimization
-    #         level set by the option -o.
-    #         The effect of this option is defined by the zlib(3) library used by OptiPNG.
-    #  -zs strategies
-    #         Select the zlib compression strategies used in IDAT compression.
-    #         The strategies argument is specified as a rangeset (e.g. -zs0-3), and the default strategies value depends on the optimiza-
-    #         tion level set by the option -o.
-    #         The effect of this option is defined by the zlib(3) library used by OptiPNG.
-    #  -zw size
-    #         Select the zlib window size (32k,16k,8k,4k,2k,1k,512,256) used in IDAT compression.
-    #         The size argument can be specified either in bytes (e.g. 16384) or kilobytes (e.g. 16k). The default size value is  set  to
-    #         the lowest window size that yields an IDAT output as big as if yielded by the value 32768.
-    #         The effect of this option is defined by the zlib(3) library used by OptiPNG.
-    # zc = 9  zm = 8  zs = 0  f = 5
-    cmd = "optipng -zc 9 -zm 8 -zs 0 -f 5 -o1 #{image} >> optipng.log"
-    system(cmd)
+      puts "  #{start_idx} - #{end_idx}"
+
+      pdf_name   = "#{start_idx}-#{end_idx}-cmyk.pdf"
+      slice_glob = "print/*_{#{start_idx}..#{end_idx}}_*.tiff"
+
+      puts "slice_glob #{slice_glob}"
+      bundle_command = "convert #{slice_glob} final/#{pdf_name}"
+      puts "bundle .tiff files into PDF: #{bundle_command}"
+      system(bundle_command)
+    end
   end
-end
 
-optimizer.shutdown
-optimizer.wait_for_termination
+  bundler.shutdown
+  bundler.wait_for_termination
+end
 
 fin = Time.now
 puts "GENERATED IN #{fin.to_f - start.to_f} SECONDS"
